6152294997034b75e93cac9a3180ea3014b7d2e9
Dionatan <dionatanonliveira@cidadaoon.com>
2023-04-26 15:27:51 -0300

    site

--- src/components/CarroselHero.jsx
+++ src/components/CarroselHero.jsx
@@ -18,10 +18,10 @@ function CarroselHero({ slides }) {
     
 
     return (
-      <div className="carrossel relative z-[50] h-[220px]">
+      <div className="carrossel relative z-[50] ms:h-[220px] ss:h-[180px] xs:h-[130px] h-[150px]">
         <TransitionGroup className="slide-container">
           <CSSTransition key={currentIndex} timeout={500} classNames="slide">
-            <div onClick={() => nextSlide()} className="slide cursor-pointer font-raleway font-bold ss:leading-[95px] xs:leading-[80px] relative z-[10] sM:text-[90px] ss:text-[70px] xs:text-[52px] text-[37px]"
+            <div onClick={() => nextSlide()} className="slide cursor-pointer font-raleway font-bold ss:leading-[95px] xs:leading-[80px] relative z-[10] lg:text-[90px] ms:text-[70px] sm:text-[60px] ss:text-[52px] xs:text-[40px] text-[32px]"
                 style={{ whiteSpace: 'pre-wrap' }} dangerouslySetInnerHTML={{ __html: slides[currentIndex] }}></div>
           </CSSTransition>
         </TransitionGroup>
